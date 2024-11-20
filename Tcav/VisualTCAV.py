3a9e43f5fbd354ab77963089198efdda9d43e6e9
DanieleDiSanti <daniele.disanti@mail.polimi.it>
2024-11-20 15:33:31 +0100

    letsgoski!

--- Tcav/VisualTCAV.py
+++ Tcav/VisualTCAV.py
@@ -11,12 +11,13 @@
 # Imports
 #####
 
-from Torch_VisualTCAV.utils import cosine_similarity, nth_highest_index, contraharmonic_mean
-from Torch_VisualTCAV.LocalVisualTCAV import LocalVisualTCAV
-
-
 # Do not generate "__pycache__" folder
 import sys
+
+from Tcav.LocalVisualTCAV import LocalVisualTCAV
+from Tcav.TorchModel import Model
+from Tcav.utils import Predictions, Prediction, ConceptLayer, contraharmonic_mean
+
 sys.dont_write_bytecode = True
 
 import os
@@ -43,15 +44,14 @@ import seaborn as sns
 # 3 = INFO, WARNING, and ERROR messages are not printed
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import tensorflow as tf
 #import tensorflow_probability as tfp
 
-import pyTorch as torch
 import os
 import torch
 import torch.nn.functional as F
 from joblib import dump, load
 import torch
+import torchvision
 import torchvision.transforms as transforms
 from PIL import Image
 import os
@@ -68,11 +68,18 @@ preprocess_resnet_v2 = torchvision.transforms.Normalize(mean=IMAGENET_MEAN, std=
 #preprocess_v3 = tf.keras.applications.inception_v3.preprocess_input
 #preprocess_vgg16 = tf.keras.applications.vgg16.preprocess_input
 
+def get_model_by_name(model_name):
+	if model_name == 'RESNET50':
+		model_graph_path = 'Models/RESNET50/Resnet50_V2.pth'
+		model_labels_path = 'Models/RESNET50/ResNet50V2-imagenet-classes.txt'
+		return Model(model_name, model_graph_path, model_labels_path)
+
+
 #####
 # VisualTCAV class
 #####
 
-class VisualTCAV():
+class VisualTCAV:
 
 	##### Init #####
 	def __init__(
@@ -97,11 +104,12 @@ class VisualTCAV():
 		os.makedirs(self.concept_images_dir, exist_ok=True)
 
 		self.batch_size = batch_size
+		self.predictions = []
 
 		# Model
 		self.model = None
 		if model:
-			self._bindModel(model)
+			self._bind_model(model)
 
 		if clear_cache:
 			for file in os.listdir(self.cache_dir):
@@ -116,14 +124,14 @@ class VisualTCAV():
 		self.random_acts = {}
 
 	# Set a list of concepts
-	def setConcepts(self, concept_names):
+	def set_concepts(self, concept_names):
 		self.concepts = []
 		for concept_name in concept_names:
 			if concept_name not in self.concepts:
 				self.concepts.append(concept_name)
 
 	# Set a list of layers
-	def setLayers(self, layer_names):
+	def set_layers(self, layer_names):
 		self.layers = []
 		for layer_name in layer_names:
 			if layer_name not in self.layers:
@@ -139,15 +147,11 @@ class VisualTCAV():
 			raise Exception("Please instantiate a Model first")
 
 		# Predict with the provided model wrapper
-		self.predictions = self.model.model_wrapper.get_predictions(
-			self.model.preprocessing_function(
-				self.resized_imgs
-			)
-		)
+		self.predictions = self.model.model_wrapper.get_predictions(self.resized_imgs)
 
 		# Sort & add class names
 		self.predictions = np.array([
-			self._sortTargetClasses(
+			self._sort_target_classes(
 				prediction,
 				self.model.model_wrapper.id_to_label,
 				no_sort
@@ -163,7 +167,7 @@ class VisualTCAV():
 	#####
 
 	# Bind a model
-	def _bindModel(self, model):
+	def _bind_model(self, model):
 
 		# Folders and directories
 		model.graph_path_dir = os.path.join(self.models_dir, model.model_name, model.graph_path_filename)
@@ -189,7 +193,7 @@ class VisualTCAV():
 		self.model = model
 
 	# Reshape a list of predictions
-	def _sortTargetClasses(self, predictions, id_to_label, no_sort=False):
+	def _sort_target_classes(self, predictions, id_to_label, no_sort=False):
 
 		# Reshape
 		indexed_arr = list(enumerate(predictions))
@@ -220,25 +224,24 @@ class VisualTCAV():
 		grads = torch.stack(grads)  # Assicuriamoci che grads sia un tensore di PyTorch
 		avg_grads = torch.mean((grads[:-1] + grads[1:]) / 2.0, dim=0)
 
-    return avg_grads
-
+		return avg_grads
 
 	# Utils function to interpolate the feature maps
 	def _interpolate_images(self, feature_maps, baseline, alphas):
-		#----TO TEST----
-    # Converting feature_maps to float32
-    image = feature_maps.float()
+			#----TO TEST----
+		# Converting feature_maps to float32
+		image = feature_maps.float()
 
-    # Expanding dimensions to match the shape of the tensors
-    alphas_x = alphas.view(-1, 1, 1, 1)
-    baseline_x = baseline.unsqueeze(0)
-    input_x = image.unsqueeze(0)
+		# Expanding dimensions to match the shape of the tensors
+		alphas_x = alphas.view(-1, 1, 1, 1)
+		baseline_x = baseline.unsqueeze(0)
+		input_x = image.unsqueeze(0)
 
-    # Calculating the delta and interpolated images
-    delta = input_x - baseline_x
-    images = baseline_x + alphas_x * delta
+		# Calculating the delta and interpolated images
+		delta = input_x - baseline_x
+		images = baseline_x + alphas_x * delta
 
-    return images
+		return images
 
 	# Function to compute the negative examples activations for a given layer
 	def _compute_random_activations(self, cache, layer_name):
@@ -252,7 +255,7 @@ class VisualTCAV():
 
 			# If cache is requested
 			if cache:
-				dump(random_acts, cache_random_acts_path, compress=9)
+				dump(random_acts, cache_random_acts_path, compress=3)
 		# Return
 		return random_acts
 
@@ -264,58 +267,58 @@ class VisualTCAV():
 		)
 		return feature_maps_for_concept
 
-
 	# Function to compute the CAV given a concept & a layer
 	def _compute_cavs(self, cache, concept_name, layer_name, random_acts):
+
 		#---TO TEST---
 		# Define cache path
-    cache_path = os.path.join(self.cache_dir, f'cav_{concept_name}_{self.model.max_examples}_{self.random_images_folder}_{layer_name}.joblib')
+		cache_path = os.path.join(self.cache_dir, f'cav_{concept_name}_{self.model.max_examples}_{self.random_images_folder}_{layer_name}.joblib')
 
-    # Check if cache exists
-    if cache and os.path.isfile(cache_path):
-        concept_layer = load(cache_path)
-    else:
-        # Activations (concept/layer)
-        concept_acts = self.model.activation_generator.get_feature_maps_for_concept(concept_name, layer_name)
+		# Check if cache exists
+		if cache and os.path.isfile(cache_path):
+			concept_layer = load(cache_path)
+		else:
+			# Activations (concept/layer)
+			concept_acts = self.model.activation_generator.get_feature_maps_for_concept(concept_name, layer_name)
 
-        # Pooling the concept and random activations along spatial dimensions
-        pooled_concept = concept_acts.mean(dim=(1, 2))
-        pooled_random = random_acts.mean(dim=(1, 2))
+			# Pooling the concept and random activations along spatial dimensions
+			pooled_concept = concept_acts.mean(dim=(1, 2))
+			pooled_random = random_acts.mean(dim=(1, 2))
 
-        # Initialize ConceptLayer
-        concept_layer = ConceptLayer()
+			# Initialize ConceptLayer
+			concept_layer = ConceptLayer()
 
-        # Calculate centroids
-        concept_layer.cav.centroid0 = pooled_concept.mean(dim=0)
-        concept_layer.cav.centroid1 = pooled_random.mean(dim=0)
-        concept_layer.cav.direction = concept_layer.cav.centroid0 - concept_layer.cav.centroid1
+			# Calculate centroids
+			concept_layer.cav.centroid0 = pooled_concept.mean(dim=0)
+			concept_layer.cav.centroid1 = pooled_random.mean(dim=0)
+			concept_layer.cav.direction = concept_layer.cav.centroid0 - concept_layer.cav.centroid1
 
-        # Emblems computation
-        emblems = contraharmonic_mean(
-            F.relu(
-                (concept_layer.cav.direction[None, None, None, :] * concept_acts).sum(dim=3)
-            ),
-            axis=(1, 2)
-        )
+			# Emblems computation
+			emblems = contraharmonic_mean(
+				F.relu(
+					(concept_layer.cav.direction[None, None, None, :] * concept_acts).sum(dim=3)
+				),
+				axis=(1, 2)
+			)
 
-        negative_emblems = contraharmonic_mean(
-            F.relu(
-                (concept_layer.cav.direction[None, None, None, :] * random_acts).sum(dim=3)
-            ),
-            axis=(1, 2)
-        )
+			negative_emblems = contraharmonic_mean(
+				F.relu(
+					(concept_layer.cav.direction[None, None, None, :] * random_acts).sum(dim=3)
+				),
+				axis=(1, 2)
+			)
 
-        # Convert emblems to float tensors
-        concept_layer.cav.concept_emblem = torch.tensor([
-            torch.quantile(emblems, 0.5),
-            torch.quantile(negative_emblems, 0.5)
-        ], dtype=torch.float32)
+			# Convert emblems to float tensors
+			concept_layer.cav.concept_emblem = torch.tensor([
+				torch.quantile(emblems, 0.5),
+				torch.quantile(negative_emblems, 0.5)
+			], dtype=torch.float32)
 
-        print(concept_layer.cav.concept_emblem)
+			print(concept_layer.cav.concept_emblem)
 
-        # Cache the computed concept layer if requested
-        if cache:
-            dump(concept_layer, cache_path, compress=9)
+			# Cache the computed concept layer if requested
+			if cache:
+				dump(concept_layer, cache_path, compress=9)
 
-    # Return the computed concept layer
-    return concept_layer
+		# Return the computed concept layer
+		return concept_layer
